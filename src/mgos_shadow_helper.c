28b86bc7c99bc2abaa271aaaafcc86e6bd0982af
Chris Brand <chris@ergosense.co.za>
2018-07-25 00:02:56 +0200

    Treat accepted and delta event differntly

--- src/mgos_shadow_helper.c
+++ src/mgos_shadow_helper.c
@@ -6,6 +6,10 @@
 
 static void _shadow_cb(int ev, void *evd, void *arg)
 {
+  // Extract the delta
+  char* delta;
+  int extracted = 0;
+
   switch (ev)
   {
     case MGOS_SHADOW_CONNECTED:
@@ -13,8 +17,19 @@ static void _shadow_cb(int ev, void *evd, void *arg)
       mgos_shadow_helper_write(NULL);
       break;
     case MGOS_SHADOW_GET_ACCEPTED:
+      extracted = json_scanf(((struct mg_str*) evd)->p, ((struct mg_str*) evd)->len, "{ desired: %Q }", &delta);
+
+      if (extracted)
+      {
+        LOG(LL_INFO, ("Device delta extracted %s (%d)", delta, strlen(delta)));
+        mgos_shadow_helper_sync(delta);
+        free(delta);
+      }
+
+      break;
     case MGOS_SHADOW_UPDATE_DELTA:
-      LOG(LL_INFO, ("Device state received %s (%d)", ((struct mg_str*) evd)->p, ((struct mg_str*) evd)->len));
+      // This event only contains the delta information
+      LOG(LL_INFO, ("Device delta received %s (%d)", ((struct mg_str*) evd)->p, ((struct mg_str*) evd)->len));
       mgos_shadow_helper_sync(((struct mg_str*) evd)->p);
       break;
   }
